51b46c5b15eb30d1f9a5d969169dd8beba275548
Slanfan <slanfan@gmail.com>
2014-12-10 20:51:48 +0100

    update

--- www/js/index.js
+++ www/js/index.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
         error: function(response) {
             // nothing
         }
-    }, function () {
+    }).done(function () {
         $("#booster").slick({
             dots: false,
             arrows: false
