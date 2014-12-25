208818be9d23cd6f7db36ea56ffeb9b8a244cefa
Slanfan <slanfan@gmail.com>
2014-12-26 00:03:53 +0100

    Update

--- www/js/index.js
+++ www/js/index.js
@@ -86,7 +86,7 @@ function pickCard(id) {
     var Multiverseid = $("#card-" + id).data("card-id");
     var Manacost = '';
     
-    if (Color === 'L') {
+    if (Color === 'l') {
         // Nothing
     } else {
         var result = Cost.match(/[^{}]+/g);
